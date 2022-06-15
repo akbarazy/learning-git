73ad4d5d1678634054ad044ca3591a70b518a7bc
Akbar <tes@gmail.com>
2022-06-15 10:03:32 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Akbarazy | Our List</title>
+
+    <link rel="stylesheet" href="static/css/bootstrap.min.css">
+</head>
+
+<body>
+    <!-- section header -->
+    <p>Hello World</p>
+
+    <script src="static/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="static/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
